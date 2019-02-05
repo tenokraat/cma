9f05dfd226bf27d12ae3a7853dfa5a4262643ec7
Oliver Wehrli <wehrli@cbcng.local>
2019-02-05 15:08:40 +0100

    commit

--- aruba-aos8-geo-location/example_get_node_hierarchy.py
+++ aruba-aos8-geo-location/example_get_node_hierarchy.py
@@ -4,17 +4,23 @@ import json,requests
 from aruba_api_caller import *
 
 session = api_session("cs-aruba-mm.hpearubademo.com", "admin", "Adminhpq-123")
+isEmpty = False
 
 session.login()
 
 cd = session.get("configuration/object/node_hierarchy")
 
-#md_name = cd['childnodes'][1]['childnodes'][1]['childnodes'][1]['devices']
-
+#def checkNewDevices():
+#Check node hierarchy for devices that have not been renamed, yet.
 for each in cd['childnodes'][1]['childnodes'][1]['childnodes'][1]['devices']:
-    print ('Hostname: '+each['name']+' MAC: '+each['mac'])
- #   print(each['mac'])
+    if each['name'] == each['mac']:
+     print ('Hostname: '+each['name']+' MAC: '+each['mac'])
+    else:
+        isEmpty = True
 
-#print (md_name)
-
+if isEmpty: 
+    print ('No new devices found.')
+else:
+    print('There are new devices detected.')
+  
 session.logout()
