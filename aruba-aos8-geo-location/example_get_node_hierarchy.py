233ab0f4d6f9b3fe14e862133be24d59605fb301
Oliver Wehrli <wehrli@cbcng.local>
2019-02-05 09:41:00 +0100

    fetch hostname and mac

--- aruba-aos8-geo-location/example_get_node_hierarchy.py
+++ aruba-aos8-geo-location/example_get_node_hierarchy.py
@@ -7,8 +7,18 @@ session = api_session("cs-aruba-mm.hpearubademo.com", "admin", "Adminhpq-123")
 
 session.login()
 
-cd = session.get("configuration/object/node_hierarchy", "")
+cd = session.get("configuration/object/node_hierarchy")
 
-print (cd)
+output = cd['name']
+
+print ('Node Hierarchy Name: '+output)
+
+#md_name = cd['childnodes'][1]['childnodes'][1]['childnodes'][1]['devices']
+
+for each in cd['childnodes'][1]['childnodes'][1]['childnodes'][1]['devices']:
+    print (each['name']) 
+    print(each['mac'])
+
+#print (md_name)
 
 session.logout()
