599e59a16692bdb41622916368e14d44e5b27c0b
Oliver Wehrli <wehrli@cbcng.local>
2019-02-05 20:07:19 +0100

    remove relative import

--- example_get_node_hierarchy.py
+++ example_get_node_hierarchy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import json,requests
-from .aruba_api_caller import *
+from aruba_api_caller import *
 
 session = api_session("cs-aruba-mm.hpearubademo.com", "admin", "Adminhpq-123")
 isEmpty = False
