7cf18c23bca862471e9ddfec347ec0598bfa7eba
Oliver Wehrli <wehrli@cbcng.local>
2019-03-18 16:05:10 +0100

    added class to tomtom_geolocation

--- tomtom_geolocation.py
+++ tomtom_geolocation.py
@@ -1,58 +1,59 @@
 #!/usr/bin/python3
 
-import json,requests, os
+import json,requests,os
 
-#This Script needs to connect to the internet, hence requires proxy.
+class tomtom_geolocation:
+    def __init__ (self):
+        #This class needs to connect to the internet, hence requires proxy.
 
-cma_proxy = 'http://192.168.205.163:8080/'
-os.environ['http_proxy'] = cma_proxy 
-os.environ['https_proxy'] = cma_proxy
+        cma_proxy = 'http://192.168.205.163:8080/'
+        os.environ['http_proxy'] = cma_proxy 
+        os.environ['https_proxy'] = cma_proxy
 
-#Using Python requests and TomTom Search/Geocode API to retrieve geo location.
+        #Using Python requests and TomTom Search/Geocode API to retrieve geo location.
 
-#API Key oliver.wehrli@gmail.com
+        #API Key oliver.wehrli@gmail.com
 
-api_key = 'KR3oOGSM59aMJBOSKAxTlxoJvYBjrENU'
+        self.api_key = 'KR3oOGSM59aMJBOSKAxTlxoJvYBjrENU'
+
+    def get_geolocation(self, shop_address, api_key):
+
+        #TomTom API query syntax /search/{versionNumber}/geocode/{query}.{ext}
+        #https://developer.tomtom.com/content/search-api-explorer
 
 
-def get_geolocation(shop_address):
+        #Restrict TomTom search to Switzerland
+        tomtom_search_params = {
+            'countrySet': 'CH',
+            'lat': '46.204391',
+            'lon': '6.143158',
+            'key': api_key
+        }
 
-    #TomTom API query syntax /search/{versionNumber}/geocode/{query}.{ext}
-    #https://developer.tomtom.com/content/search-api-explorer
+        geocode_api_url = f'https://api.tomtom.com/search/2/geocode/{shop_address}.json'
 
+        # Do the request to TomTom URL, pass search parameters and retrieve the response data
+        req = requests.get(geocode_api_url, params=tomtom_search_params)
+        print (req.url)
 
-    #Restrict TomTom search to Switzerland
-    tomtom_search_params = {
-        'countrySet': 'CH',
-        'lat': '46.204391',
-        'lon': '6.143158',
-        'key': api_key
-    }
+        res = req.json()
+        print (res)
 
-    geocode_api_url = f'https://api.tomtom.com/search/2/geocode/{shop_address}.json'
+        summary = res['summary']
+        print (summary)
 
-    # Do the request to TomTom URL, pass search parameters and retrieve the response data
-    req = requests.get(geocode_api_url, params=tomtom_search_params)
-    print (req.url)
+        # Use the first result
+        result = res['results'][0]
+        print (result)
 
-    res = req.json()
-    print (res)
+        geodata = dict()
+        
+        geodata['lat'] = result['position']['lat']
+        geodata['lon'] = result['position']['lon']
+        geodata['address'] = result['address']['freeformAddress']
 
-    summary = res['summary']
-    print (summary)
+        print('{address}. (lat, lng) = ({lat}, {lon})'.format(**geodata))
 
-    # Use the first result
-    result = res['results'][0]
-    print (result)
+        return geodata
 
-    geodata = dict()
-    
-    geodata['lat'] = result['position']['lat']
-    geodata['lon'] = result['position']['lon']
-    geodata['address'] = result['address']['freeformAddress']
-
-    print('{address}. (lat, lng) = ({lat}, {lon})'.format(**geodata))
-
-    return geodata
-
-#get_geolocation('Hegenheimermattweg 65 4123 Allschwil')
+    #get_geolocation('Hegenheimermattweg 65 4123 Allschwil')
