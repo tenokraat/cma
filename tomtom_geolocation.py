e5fbfd6ea695cd0838c9b3e303e0478e51a7e0df
Oliver Wehrli <wehrli@cbcng.local>
2019-03-18 15:45:54 +0100

    corrected shop_address variable in tomtom_geolocation

--- tomtom_geolocation.py
+++ tomtom_geolocation.py
@@ -29,7 +29,7 @@ def get_geolocation(shop_address):
         'key': api_key
     }
 
-    geocode_api_url = f'https://api.tomtom.com/search/2/geocode/{address}.json'
+    geocode_api_url = f'https://api.tomtom.com/search/2/geocode/{shop_address}.json'
 
     # Do the request to TomTom URL, pass search parameters and retrieve the response data
     req = requests.get(geocode_api_url, params=tomtom_search_params)
