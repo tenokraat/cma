feab9103f67677fdab1f504f113415e3bb0c828e
Oliver Wehrli <wehrli@cbcng.local>
2019-03-19 19:20:20 +0100

    commit

--- cma_rollout.py
+++ cma_rollout.py
@@ -34,6 +34,7 @@ def firmware_upgrade(mac_addr, aos_compliance_version, scp_server, scp_user, scp
             'imagehost': scp_server,
             'username': scp_user,
             'image-path': '.',
+            'partition': false,
             'passwd': scp_password
         }
     
