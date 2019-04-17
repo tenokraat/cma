6d25f592bd71280451c490d905d9ed1ecc657b99
tenokraat <33875465+tenokraat@users.noreply.github.com>
2019-04-18 00:12:44 +0900

    changed username

--- srvma230-026_reset_sa.py
+++ srvma230-026_reset_sa.py
@@ -10,7 +10,7 @@ f = open ('/home/aruba/environments/cma/reset-logs/srvma230-026-reset-log.txt', 
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.230.127', username='admin', password='Aruba1234')
+ssh.connect('192.168.230.127', username='python', password='Aruba1234')
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 chan = ssh.invoke_shell()
 
